af094eb3223503ea610d9a82348ce05039fbf8e8
austincrisp <austi@AUSTINBLADE>
2017-02-10 13:25:02 -0500

    Add project files.

---
+++ Meeeedium/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Meeeedium.MvcApplication" Language="C#" %>
---
+++ Meeeedium/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Meeeedium.Startup))]
+namespace Meeeedium
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
